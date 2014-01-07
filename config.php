88f442d4073fa9ef8f41234e5147c39217b30206
unknown <ruckt@RUCKT1.emea.hpqcorp.net>
2014-01-07 15:48:00 +0100

    Nothing special.

--- config.php
+++ config.php
@@ -1,3 +1,5 @@
 <?php
 $company_name = "JemLine";
+
+$page_title = "JemLine Online";
 ?>
--- index.php
+++ index.php
@@ -3,11 +3,18 @@ include 'config.php';
 ?>
 <html>
 <head>
-
+<title><?php echo $page_title; ?></title>
+<link ref="stylesheet" href="css/styles.css" />
 </head>
 <body>
-
+<div class="header">
+<?php
+include 'header.php';
+?>
+</div>
 <h1><?php echo $company_name; ?></h1>
+
+</div>
 
 </body>
 </html>
