b3c1033456534f71d591be819ef80da4e253f5de
MaxxMaxxMaxx <maxim_sermin@yahoo.de>
2014-01-09 09:27:20 +0100

    scaled pictures

---
+++ rings.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>JemLine</title>
+<link rel="stylesheet" type="text/css" href="assets/main.css" />
+</head>
+
+<body>
+
+<div class="container">
+  <div class="header">
+	<a href="main.php"><img src="assets/logo_small.png" class="logo" alt="Insert Logo Here" name="Insert_logo" width="180" height="90" id="Insert_logo" /></a> 
+	<ul class="tabs">
+	<li><a href="categories.php"><span>C</span>ATEGORIES</a></li>
+	<li><a href="#"><span>S</span>ALES</a></li>
+	<li><a href="#"><span>H</span>ISTORY</a></li>
+	</ul>
+	<!-- end .header --></div>
+  <div class="content" style="clear: both;">
+  
+  <div class = "ring1">
+
+	<p>3/4 ct. Princess Cut Diamond Solitaire</br>Engagement Ring in 18k White Gold </p>
+
+	<img src="assets/ring1.jpg" width="350"/>
+	
+		<ul>
+		<li><span>18k White gold</span></li>
+		<li>Conflict Free diamonds</li>
+		<li>100% natural diamonds</li>
+		</ul>
+	
+  </div>
+
+
+
+
+<!-- end .content --></div>
+  <div class="footer">
+    <p>Footer</p>
+    <!-- end .footer --></div>
+  <!-- end .container --></div>
+</body>
+</html>
