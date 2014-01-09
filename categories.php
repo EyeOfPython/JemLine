d841b8f1f35911589c94e38c3fc541266171c272
unknown <ruckt@RUCKT1.emea.hpqcorp.net>
2014-01-09 09:52:45 +0100

    Made changes in

--- categories.php
+++ categories.php
@@ -1,41 +1,14 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>JemLine</title>
-<link rel="stylesheet" type="text/css" href="assets/main.css" />
-</head>
 
-<body>
+<h1>Welcome to the categories!</h1>
 
-<div class="container">
-  <div class="header">
-	<a href="main.php"><img src="assets/logo_small.png" class="logo" alt="Insert Logo Here" name="Insert_logo" width="180" height="90" id="Insert_logo" /></a> 
-	<ul class="tabs">
-	<li><a href="categories.php"><span>C</span>ATEGORIES</a></li>
-	<li><a href="#"><span>S</span>ALES</a></li>
-	<li><a href="#"><span>H</span>ISTORY</a></li>
-	</ul>
-	<!-- end .header --></div>
-  <div class="content" style="clear: both;">
-  <h1>Welcome to the categories!</h1>
+  <table cellspacing="10" cellpadding="20" class = "categories">
+	<tr>
+		<td><a href="rings.php">Rings</a></td>
+		<td>Earrings</td>
+		<td>Necklaces</td>
+	</tr>
+	<tr>
+		<td>Bracelets</td>
+	</tr>
+  </table>
 
-      <table cellspacing="10" cellpadding="20" class = "categories">
-        <tr>
-        	<td><a href="rings.php">Rings</a></td>
-            <td>Earrings</td>
-            <td>Necklaces</td>
-        </tr>
-		<tr>
-        	<td>Bracelets</td>
-        </tr>
-      </table>
-
-
-<!-- end .content --></div>
-  <div class="footer">
-    <p>Footer</p>
-    <!-- end .footer --></div>
-  <!-- end .container --></div>
-</body>
-</html>
